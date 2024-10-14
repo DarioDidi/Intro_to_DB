eb5dbeae36d2d9a5b2e0033e2bec882bcd0c1f38
DD-tech <>
2024-10-14 19:34:43 +0300

    added task5, smeicolon task4

---
+++ task_5.sql
@@ -0,0 +1,12 @@
+-- customer_id = 1
+-- customer_name = Cole Baidoo
+-- email = cbaidoo@sandtech.com
+-- address = 123 Happiness Ave.
+-- INSERT INTO TableName (ID, Text, OtherColumns...) Values (99, 'foo', ...)
+    
+    
+    
+    
+
+
+INSERT INTO customers (customer_id, customer_name, email, address) VALUES (1, Cole Baidoo, cbaidoo@sandtech.com, 123 Happiness Ave.);
