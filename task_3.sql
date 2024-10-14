5cd8bb086953ccdc1445024dad21657648d64b2d
DD-tech <>
2024-10-14 13:19:15 +0300

    added task3

---
+++ task_3.sql
@@ -0,0 +1,11 @@
+-- SELECT TABLE_NAME 
+-- FROM INFORMATION_SCHEMA.TABLES
+-- WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='dbName'
+
+DECLARE @mydb NVARCHAR(50);
+set @mydb ='alx_book_store';
+USE @mydb
+GO 
+SELECT *
+FROM sys.Tables
+GO
