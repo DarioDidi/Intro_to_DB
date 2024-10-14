3492956440cc213c1937b3d08bca60c462853308
DD-tech <>
2024-10-14 12:29:42 +0300

    changed foreign key declaration in orders table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -44,7 +44,8 @@ CREATE TABLE
 CREATE TABLE
     Orders (
         order_id INT PRIMARY KEY,
-        customer_id FOREIGN KEY REFERENCES Customers (customer_id),
+        -- customer_id FOREIGN KEY REFERENCES Customers (customer_id),
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
         order_date DATE
     );
 
