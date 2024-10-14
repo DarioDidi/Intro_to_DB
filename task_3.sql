c022c49283af2fde0d20f9ee19cea36ec8e08d57
DD-tech <>
2024-10-14 13:23:07 +0300

    explicit db name in show command

--- task_3.sql
+++ task_3.sql
@@ -2,7 +2,8 @@
 -- FROM INFORMATION_SCHEMA.TABLES
 -- WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='dbName'
 
-DECLARE @mydb NVARCHAR(50);
-set @mydb ='alx_book_store';
-USE @mydb;
+-- DECLARE @mydb NVARCHAR(50);
+-- set @mydb ='alx_book_store';
+-- USE @mydb;
+USE alx_book_store;
 SHOW TABLES;
