f7403aac9f8c7e7cce84dc1b3e2d16a0e2a044c2
DD-tech <>
2024-10-14 19:50:01 +0300

    added task 6

---
+++ task_6.sql
@@ -0,0 +1,33 @@
+-- INSERT INTO MyTable
+--   ( Column1, Column2, Column3 )
+-- VALUES
+--   ('John', 123, 'Lloyds Office'),
+--   ('Jane', 124, 'Lloyds Office'),
+--   ('Billy', 125, 'London Office'),
+--   ('Miranda', 126, 'Bristol Office');
+
+INSERT INTO
+    alx_book_store.Customer (
+        customer_id,
+        customer_name,
+        email,
+        address
+    )
+VALUES (
+        2,
+        'Blessing Malik',
+        'bmalik@sandtech.com',
+        '124 Happiness  Ave.'
+    ),
+    (
+        '3',
+        'Obed Ehoneah',
+        'eobed@sandtech.com',
+        '125 Happiness  Ave.'
+    ),
+    (
+        '4',
+        'Nehemial Kamolu',
+        'nkamolu@sandtech.com',
+        '126 Happiness  Ave.'
+    );
